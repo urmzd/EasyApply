79fa86da6189420aca6a31f2fbe886044bd701b0
Documenter.jl <documenter@juliadocs.github.io>
2022-05-27 19:22:42 +0000

    build based on 8fee6f1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = EasyApply","category":"page"},{"location":"#EasyApply","page":"Home","title":"EasyApply","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for EasyApply.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [EasyApply]","category":"page"}]
+}
